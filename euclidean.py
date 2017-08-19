444e8b89bf27069129de971157f1ddcdbd4e71a1
Mebinmaria <mebinmaria93@gmail.com>
2017-08-19 17:05:53 +0530

    Add files via upload

---
+++ euclidean.py
@@ -0,0 +1,6 @@
+a =input('enter the first point')
+b =input('enter the second point')
+c =input('enter the third point')
+d =input('enter the fourth point')
+n =  (a - c)**2 + (b - d)**2
+print n**.5
